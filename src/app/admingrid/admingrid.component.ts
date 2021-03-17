5540aef4fb13576d312a4261581ab6d94d95ce77
Jesse <donah022@cougars.csusm.edu>
2021-03-17 14:24:04 -0700

    update

--- src/app/admingrid/admingrid.component.ts
+++ src/app/admingrid/admingrid.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit,EventEmitter } from '@angular/core';
 
+import * as XLSX from 'xlsx';
+
+
 @Component({
   selector: 'app-admingrid',
   templateUrl: './admingrid.component.html',
@@ -10,6 +13,7 @@ export class AdmingridComponent implements OnInit {
   private columnApi;  //defines placeholder for our columnApi 
   myRowData = []; // Defines row definitions
   myColumnDefs = [];  //defines column definitions 
+  public excelSheet: XLSX.WorkSheet;
   public excelData: [][]; //excel matrix use to store data informations. 
   public arrayData :any;
   /* Grid Options 
@@ -42,6 +46,7 @@ export class AdmingridComponent implements OnInit {
   
     //create html button to trigger function 
     receiveData($event){
+<<<<<<< Updated upstream
       this.excelData = $event;
       this.updateCols();
       
@@ -53,6 +58,22 @@ export class AdmingridComponent implements OnInit {
       console.log(this.arrayData);
       this.gridApi.setRowData(this.arrayData);
     }
+=======
+      this.excelSheet = $event;
+      console.log(this.excelSheet)
+      console.log("inside receiveData function")
+    }
+    receiveMatrixData($event){
+      this.excelData= $event;
+      console.log("inside receiveMatrixdata function");;
+      this.updateCols();
+      console.log("finished update columns function, going into populate data function")
+      this.populateRows();
+
+    }
+   
+    //update COLS just adds columns to grid. 
+>>>>>>> Stashed changes
     updateCols()
     {
     
@@ -61,7 +82,9 @@ export class AdmingridComponent implements OnInit {
         this.myColumnDefs.push({field:this.excelData[0][i]});
       }
       this.gridApi.setColumnDefs(this.myColumnDefs);
+
     }
+<<<<<<< Updated upstream
     // i = rows, x = columns
     //this.exceldata.length = amount of rows/number of arrays
    // this.exceldata[i].length = number of columns/length of array
@@ -88,3 +111,60 @@ export class AdmingridComponent implements OnInit {
     this.myRowData.push({field:this.excelData[i][x]});
    }
    */
+=======
+    setcolumns(columns)
+    {
+      var char = 'A';
+      var num = 0;
+      var col1 = 0;
+      var col2 = 0;
+      for(var i = 1;i <=this.myColumnDefs.length;i++)
+      {
+        //save first header
+        columns[char] = this.myColumnDefs[i-1].field;
+        //check if 'i' is longer than the alphabet
+        if(i<26){
+          num = char.charCodeAt(0); //Store char as number
+          num++;
+          char = String.fromCharCode(num); //Going from ascii to char
+          console.log(char);
+        }
+        else
+        {
+          
+          col1 = (i/26)+64;
+          col2 = (i%26)+65;
+          char = String.fromCharCode(col1)+String.fromCharCode(col2);
+          console.log(char);
+        }
+      }
+
+      return columns;
+    }
+
+    populateRows()
+    {
+      console.log("inside populate rows function")
+      var columns = {};
+      columns = this.setcolumns(columns);
+      var rowData = [];
+      var rowIndex = 2;
+      while (this.excelSheet['A' + rowIndex]) {
+        var row = {};
+        Object.keys(columns).forEach(function(column) {
+            row[columns[column]] = this.excelSheet[column + rowIndex].w;
+        });
+
+        rowData.push(row);
+
+        rowIndex++;
+    }
+
+    // finally, set the imported rowData into the grid
+    this.gridApi.setRowData(rowData);
+}
+           
+         }
+
+  
+>>>>>>> Stashed changes
--- src/app/excelsheet/excelsheet.component.ts
+++ src/app/excelsheet/excelsheet.component.ts
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 import { Component, OnInit,Output,Input,EventEmitter } from '@angular/core';
+=======
+import { Grid, GridApi } from 'ag-grid-community';
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+>>>>>>> Stashed changes
 import * as XLSX from 'xlsx';
 
 @Component({
@@ -11,12 +16,19 @@ export class ExcelsheetComponent implements OnInit {
   data: [][];
    wb: XLSX.WorkBook;
   @Output() dataEvent = new EventEmitter<any>();
+<<<<<<< Updated upstream
   @Output() rowDataEvent = new EventEmitter<any>();
+=======
+  @Output() dataEvent2 = new EventEmitter<any>();
+  ws: XLSX.WorkSheet;
+
+>>>>>>> Stashed changes
   constructor() { }
   @Input() columns:[];
 
   ngOnInit(): void {
   }
+
   onFileChange(evt: any) {
     const target : DataTransfer =   <DataTransfer>(evt.target);
     
@@ -26,25 +38,31 @@ export class ExcelsheetComponent implements OnInit {
 
     reader.onload = (e: any) => {
       const bstr: string = e.target.result;
-
       const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
 
       const wsname : string = wb.SheetNames[0];
 
+<<<<<<< Updated upstream
       const ws: XLSX.WorkSheet = wb.Sheets[wsname];
       
 console.log(ws)
       this.data = (XLSX.utils.sheet_to_json(ws, { header: 1 }));
       this.populateGrid(wb);
     };
+=======
+      this.ws = wb.Sheets[wsname];
+      this.data = (XLSX.utils.sheet_to_json(this.ws, { header: 1 }));
+>>>>>>> Stashed changes
 
+    }; 
     reader.readAsBinaryString(target.files[0]);
-    
-    
+
   }
   sendData(){
-    this.dataEvent.emit(this.data);
+    this.dataEvent.emit(this.ws);
+    this.dataEvent2.emit(this.data);
   }
+<<<<<<< Updated upstream
   populateGrid(workbook :any){
     var firstSheetName = workbook.SheetNames[0];
     var worksheet = workbook.Sheets[firstSheetName];
@@ -75,3 +93,7 @@ console.log(ws)
   }
 }
 
+=======
+
+}
+>>>>>>> Stashed changes
