bc745bc9642a1a1d7afb93c27cc32a6d77aaa87b
BartoAmigo <falza001@cougars.csusm.edu>
2021-04-10 14:35:02 -0700

    adding column functionality

--- src/services/cols-from-excel.service.ts
+++ src/services/cols-from-excel.service.ts
@@ -31,37 +31,10 @@ getColumnsFromExcelFile(excelWorkSheet):any{
         col2 = (i%26)+65;
         char = String.fromCharCode(col1)+String.fromCharCode(col2);
       }
-      console.log(i);
     }
-    console.log(columns);
-    console.log(columnsForRows);
   return {columns,columnsForRows}
   }
 }
-  
 
-  /*let char = "A"; 
-  let num = 0; 
-  let col1 = 0; 
-  let col2 = 0;
-  let columns = [];
-  let columnsForRows = [];
-  for(var i = 0;i <excelData[0].length;i++)
-  {
-    columns.push({field:excelData[0][i]});
-    columnsForRows[char] = columns[i].field;
-    if(i<26){
-      num = char.charCodeAt(0);
-      num++;
-      char = String.fromCharCode(num);
-    }
-    else
-    {
-      col1 = (i/26) + 64;
-      col2 = (i%26) + 65; 
-      char = String.fromCharCode(col1) + String.fromCharCode(col2); 
-    }
-  }
-  */
 
 
