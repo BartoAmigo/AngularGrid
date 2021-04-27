3a4de4646169e0f154041c872e9f269b0a95419a
David Falconer <falco019@cougars.csusm.edu>
2021-04-27 01:13:00 -0700

    Added dropdown box to Grid Controls

--- src/app/adminpage/adminpage.component.ts
+++ src/app/adminpage/adminpage.component.ts
@@ -1,4 +1,5 @@
 
+import { state } from '@angular/animations';
 import { Component,OnInit,ViewChildren,QueryList, Input} from '@angular/core';
 import * as XLSX from "xlsx";
 import { AdmingridComponent } from '../admingrid/admingrid.component';
@@ -80,12 +81,11 @@ export class AdminpageComponent implements OnInit {
   controlClick($event){
     var list = $event.srcElement.parentNode.parentNode;
     var money = $event;
-    console.log($event);
     if (list.style.overflow === "auto") {
-      list.setAttribute("style","height:30px;width:15%;overflow:hidden;margin:9px auto");
+      list.setAttribute("style","height:30px;width:15%;overflow:hidden;margin:auto");
     }
     else {
-      list.setAttribute("style","height:fit-content;width:25%;overflow:auto;margin:9px auto");
+      list.setAttribute("style","height:fit-content;width:25%;overflow:auto;margin:auto");
     }
   }
 
@@ -93,5 +93,34 @@ export class AdminpageComponent implements OnInit {
     this.ifGridControlBox = !this.ifGridControlBox;
     $event.srcElement.classList.toggle("active");
     
+  }
+
+  controlChange(){
+    const form = <HTMLFormElement>(document.querySelector("#controls"));
+
+      const data = new FormData(form);
+      const choice = data.get('choice') as string;
+      console.log(choice);
+      switch(choice)
+      {
+        case ("export"):{
+          this.exportState();
+          break;
+        }
+        case ("reset"):{
+          this.resetGrid();
+          break;
+        }
+        case ("addrow"):{
+          this.addARow();
+          break;
+        }
+        case ("deleterow"):{
+          this.deleteRow();
+          break;
+        }
+      }
+    
+    return false;
   }
 }
